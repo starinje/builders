966689f32b2aed4c32460d148165bf6c102ec533
starinje <starinje@gmail.com>
2017-12-05 16:52:25 -0500

    hello world

--- test.py
+++ test.py
@@ -1,35 +1,36 @@
 import time
 import grovepi
 
+print "hello world"
+
 # The electromagnet can hold a 1KG weight
 
 # Connect the Grove Electromagnet to digital port D4
 # SIG,NC,VCC,GND
-electromagnet = 4
+# electromagnet = 4
 
 # something
 
-grovepi.pinMode(electromagnet,"OUTPUT")
-time.sleep(1)
+# grovepi.pinMode(electromagnet,"OUTPUT")
+# time.sleep(1)
 
-while True:
-  try:
-    # Switch on electromagnet
-    grovepi.digitalWrite(electromagnet,1)
-    print "on"
-    time.sleep(2)
+# while True:
+#   try:
+#     # Switch on electromagnet
+#     grovepi.digitalWrite(electromagnet,1)
+#     print "on"
+#     time.sleep(2)
 
-    # Switch off electromagnet
-    grovepi.digitalWrite(electromagnet,0)
-    print "off"
-    time.sleep(2)
+#     # Switch off electromagnet
+#     grovepi.digitalWrite(electromagnet,0)
+#     print "off"
+#     time.sleep(2)
 
-  except KeyboardInterrupt:
-    grovepi.digitalWrite(electromagnet,0)
-    break
-  except IOError:
-    print "Error"
+#   except KeyboardInterrupt:
+#     grovepi.digitalWrite(electromagnet,0)
+#     break
+#   except IOError:
+#     print "Error"
 
 
 
-lsdkjhsldkjahlakjdhlasdj
