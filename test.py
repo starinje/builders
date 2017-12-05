977a7b6a2efc34061296eb1151c7182be6ccaec6
starinje <starinje@gmail.com>
2017-12-05 17:09:54 -0500

    testing two em's

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ print "hello world"
 
 # Connect the Grove Electromagnet to digital port D4
 # SIG,NC,VCC,GND
-electromagnet = 7
+electromagnet_1 = 7
+electromagnet_2 = 8
 
 # something
 
@@ -17,14 +18,15 @@ time.sleep(1)
 while True:
   try:
     # Switch on electromagnet
-    grovepi.digitalWrite(electromagnet,1)
-    print "on"
-    time.sleep(2)
+    grovepi.digitalWrite(electromagnet_1,1)
+    grovepi.digitalWrite(electromagnet_2,1)
+    print "turning on"
+    time.sleep(100)
 
     # Switch off electromagnet
-    grovepi.digitalWrite(electromagnet,0)
-    print "off"
-    time.sleep(2)
+    # grovepi.digitalWrite(electromagnet,0)
+    # print "off"
+    # time.sleep(2)
 
   except KeyboardInterrupt:
     grovepi.digitalWrite(electromagnet,0)
